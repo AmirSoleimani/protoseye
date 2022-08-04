08515e952da17cdfb22bbb6afe439f183a29ae18
Amir Soleimani <amir.so@icloud.com>
2022-08-04 20:09:10 +0200

    Init cmd

---
+++ cmd/protoc-gen-protoseye/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/AmirSoleimani/protoseye/internal/protoparser"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+type Flags struct {
+	PathPrefix string
+}
+
+var f = Flags{}
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+
+	var fs flag.FlagSet
+	fs.StringVar(&f.PathPrefix, "path_prefix", "/rpc", "")
+
+	protoOpts := protogen.Options{ParamFunc: fs.Set}
+	protoOpts.
+		Run(func(plugin *protogen.Plugin) error {
+			for _, file := range plugin.Files {
+				if !file.Generate {
+					continue
+				}
+
+				for _, service := range file.Services {
+					log.Println("processing:", file.Desc.Path())
+
+					protoParser, err := protoparser.New()
+					if err != nil {
+						return err
+					}
+
+					// Generate OpenAPI spec for the service.
+					rpcs, err := protoParser.RegisterService(service)
+					if err != nil {
+						return fmt.Errorf("An error occurred while processing the service: %v", err)
+					}
+
+					out := fmt.Sprintf("%s_%s", string(file.GoPackageName), string(service.Desc.Name()))
+
+					for methodName, output := range rpcs {
+						g := plugin.NewGeneratedFile(out+"_"+methodName+"_request.json", file.GoImportPath)
+						if _, err := g.Write(output.MarshalRequest()); err != nil {
+							return err
+						}
+
+						g = plugin.NewGeneratedFile(out+"_"+methodName+"_response.json", file.GoImportPath)
+						if _, err := g.Write(output.MarshalResponse()); err != nil {
+							return err
+						}
+					}
+				}
+
+			}
+			return nil
+		})
+}
